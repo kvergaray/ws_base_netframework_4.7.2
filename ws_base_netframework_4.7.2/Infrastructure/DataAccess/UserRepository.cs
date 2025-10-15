c8ba57b1c5d363686003f71822d8f13e1063c123
Katya Vergaray Carbajal <katyavergaray@outlook.com>
2025-10-15 15:47:09 -0500

    Modificaciones commit

--- ws_base_netframework_4.7.2/Infrastructure/DataAccess/UserRepository.cs
+++ ws_base_netframework_4.7.2/Infrastructure/DataAccess/UserRepository.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using WindowsService.Domain;
+using WindowsService.Infrastructure.Helpers;
 
 namespace WindowsService.Infrastructure.DataAccess
 {
